9e246dd0481b03eae074b4837df23b29fd0a835a
BikashGithub2024 <165942928+BikashGithub2024@users.noreply.github.com>
2024-04-03 20:03:51 +0100

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,43 @@
+operation = input("enter mathematic operation u wanna perfom ? ")
+numeric1 = input("please input the first number ")
+numeric2 = input("please input the second number ")
 
+if operation == "+":
+ result = int(numeric1) + int(numeric2);
+ print(result)
+
+if operation == "-":
+ result = int(numeric1) - int(numeric2);
+ print(result)
+
+if operation == "*":
+ result = int(numeric1) * int(numeric2);
+ print(result)
+
+if operation == "/":
+ result = float(numeric1) / float(numeric2);
+ print(result)
+
+repeat_program = input("do you wanna repeat the program ?")
+if "repeat_program" == "yes":
+  print("okay")
+
+operation = input("enter mathematic operation u wanna perfom ? ")
+numeric1 = input("please input the first number ")
+numeric2 = input("please input the second number ")
+
+if operation == "+":
+ result = int(numeric1) + int(numeric2);
+ print(result)
+
+if operation == "-":
+ result = int(numeric1) - int(numeric2);
+ print(result)
+
+if operation == "*":
+ result = int(numeric1) * int(numeric2);
+ print(result)
+
+if operation == "/":
+ result = float(numeric1) / float(numeric2);
+ print(result)
